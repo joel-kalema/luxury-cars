542ae24ea89d31b873ac014ebfd86824b942717b
joel <joelkalema63@gmail.com>
2023-01-02 13:37:38 +0200

    creat a header page

---
+++ components/header.js
@@ -0,0 +1,14 @@
+export default function (){
+    return (
+        <div>
+            <div></div>
+            <ul>
+                <li>Home</li>
+                <li>cars</li>
+                <li>About</li>
+                <li>Infos</li>
+                <li>Contacts</li>
+            </ul>
+        </div>
+    )
+}
