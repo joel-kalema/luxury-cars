bf8971eaefa66ce6698bee3964e2d0c1f8f5881d
joel <joelkalema63@gmail.com>
2023-01-04 12:35:48 +0200

    fix margins

--- components/home.js
+++ components/home.js
@@ -5,11 +5,17 @@ export default function HomePage (){
                 <div className="w-3/4">
                     <div className="bg-[#fff] w-3/4 w-[0.1rem] h-20  mx-auto"></div>
                 </div>
-                <div className="mr-[-20rem]">
-                    <h1 className="text-4xl text-[#fff]">CHERON</h1>
-                    <h2 className="text-8xl text-[red]">LUXER</h2>
-                    <div className="bg-[#fff] w-3/4 h-[0.1rem] mt-20"></div>
+                <div className="mr-[-20rem] mt-60">
+                    <h1 className="text-6xl text-[#fff]">CHERON</h1>
+                    <h2 className="text-6xl text-[red]">LUXER</h2>
+                    <div className="bg-[#fff] w-3/4 h-[0.1rem] mt-40"></div>
                 </div>
+            </div>
+            <div className="flex w-4/6 justify-between">
+                <div className="1/4 justify-center items-center">hello</div>
+                <div className="1/4 justify-center items-center">hello</div>
+                <div className="1/4 justify-center items-center">hello</div>
+                <div className="1/4 justify-center items-center">hello</div>
             </div>
         </div>
     )
