8ab5a3e5c8233a95df1014c8345761f65c6dc86e
joel <joelkalema63@gmail.com>
2023-01-04 14:56:32 +0200

    create an About page

---
+++ components/about.js
@@ -0,0 +1,14 @@
+export default function About () {
+    return (
+        <div>
+            <div className="mx-auto w-2/6 py-10 md:py-28">
+                <h1 className="text-2xl">Get All Luxury Cars</h1>
+                <div className="w-1/6 h-[0.2rem] bg-[red]"></div>
+                <h1 className="text-[#fff3] text-8xl font-bold mt-[-4rem]">LUXURY</h1>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/header'
 import HomePage from '../components/home'
+import About from '../components/about'
 
 export default function Home() {
     return (
         <div>
             <Header />
             <HomePage />
+            <About />
         </div>
     )
 }
