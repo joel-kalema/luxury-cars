e1dee563f34d12a428cd648ce2431c7c09533026
joel <joelkalema63@gmail.com>
2023-01-04 07:39:00 +0200

    update a header

--- components/header.js
+++ components/header.js
@@ -1,13 +1,13 @@
 export default function Header (){
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between fixed w-5/6 exp backdrop-blur-md p-4 left-24 bg-[#fff1]">
             <div>LOGO</div>
-            <ul className="flex">
-                <li>Home</li>
-                <li>cars</li>
-                <li>About</li>
-                <li>Infos</li>
-                <li>Contacts</li>
+            <ul className="flex w-2/6 justify-between text-sm">
+                <li className='cursor-pointer hover:text-[red]'>Home</li>
+                <li className='cursor-pointer hover:text-[red]'>cars</li>
+                <li className='cursor-pointer hover:text-[red]'>About</li>
+                <li className='cursor-pointer hover:text-[red]'>Infos</li>
+                <li className='cursor-pointer hover:text-[red]'>Contacts</li>
             </ul>
         </div>
     )
