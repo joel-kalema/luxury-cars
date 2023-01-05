02dd9d8daeb4e6c272c2467b7d9fefaa1ebbc493
joel <joelkalema63@gmail.com>
2023-01-05 16:31:17 +0200

    create a cars components

---
+++ components/cars.js
@@ -0,0 +1,14 @@
+export default function Cars () {
+    return (
+        <div>
+            <div className="mx-auto w-2/6 py-10 md:py-28">
+                <h1 className="text-2xl">Witch is the No 01 Luxury cars?</h1>
+                <div className="w-1/6 h-[0.2rem] bg-[red]"></div>
+                <h1 className="text-[#fff3] text-8xl font-bold mt-[-4rem]">CARS</h1>
+            </div>
+            <div className="mx-auto">
+                
+            </div>
+        </div>
+    )
+}
