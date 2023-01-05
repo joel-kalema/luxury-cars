e29b7ddd4d21f4a4e3ae99aa7031ba1577cbc160
joel <joelkalema63@gmail.com>
2023-01-05 14:58:31 +0200

    add about content

--- components/about.js
+++ components/about.js
@@ -7,7 +7,21 @@ export default function About () {
                 <h1 className="text-[#fff3] text-8xl font-bold mt-[-4rem]">LUXURY</h1>
             </div>
             <div>
-                
+                <ul>
+                    <li>Aston Martir</li>
+                    <li>Ferrari</li>
+                    <li>Bugatti</li>
+                    <li>Maserati</li>
+                    <li>Porsche</li>
+                    <li>Bentley</li>
+                </ul>
+                <div className="w-2/6">
+                    <img src="about.jpg" alt="cars" />
+                    <div>
+                        <p>detail</p>
+                        <a href="">see detail</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
