bc64166e68bf29e5ba08456c17ee5536c178ede8
joel <joelkalema63@gmail.com>
2023-01-05 17:35:08 +0200

    add logo cars

--- components/cars.js
+++ components/cars.js
@@ -1,13 +1,14 @@
 export default function Cars () {
     return (
-        <div>
-            <div className="mx-auto w-2/6 py-10 md:py-28">
+        <div className="py-10 md:py-28">
+            <div className="mx-auto w-2/6 mb-10">
                 <h1 className="text-2xl">Witch is the No 01 Luxury cars?</h1>
-                <div className="w-1/6 h-[0.2rem] bg-[red]"></div>
-                <h1 className="text-[#fff3] text-8xl font-bold mt-[-4rem]">CARS</h1>
+                <div className="w-1/6 h-[0.2rem] bg-[red] mt-1"></div>
+                <h1 className="text-[#fff2] text-8xl font-bold mt-[-4rem]">CARS</h1>
             </div>
-            <div className="mx-auto">
-                
+            <div className="mx-auto w-4/6">
+                <p className="text-sm px-20 text-center">I can help you build a product, feature or website Look through some of my work and experience! 
+                    If you like what you see and have a project you need coded, don’t hestiate to contact me.</p>
             </div>
         </div>
     )
