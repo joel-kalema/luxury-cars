9f7a4fc720426b7e5a8e14da6367725e34761775
joel <joelkalema63@gmail.com>
2023-01-07 01:17:38 +0200

    update z-index

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 export default function Header (){
     return (
-        <div className="flex justify-between fixed w-5/6 exp backdrop-blur-md p-4 left-24 bg-[#fff1]">
+        <div className="flex justify-between fixed w-5/6 exp backdrop-blur-md p-4 left-24 bg-[#fff1] z-50">
             <div>LOGO</div>
             <ul className="flex w-2/6 justify-between text-sm">
                 <li className='cursor-pointer hover:text-[red]'>Home</li>
