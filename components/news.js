1498e9e2a7b229cf85d920f13eda1274492e7df1
joel <joelkalema63@gmail.com>
2023-01-07 01:19:11 +0200

    update news content

--- components/news.js
+++ components/news.js
@@ -1,3 +1,5 @@
+import newsData from './datas/news'
+
 export default function News () {
     return (
         <div className="py-10 md:py-28">
@@ -5,6 +7,19 @@ export default function News () {
                 <h1 className="text-2xl">News and Events</h1>
                 <div className="w-1/6 h-[0.2rem] bg-[red] mt-1"></div>
                 <h1 className="text-[#fff2] text-8xl font-bold mt-[-4rem]">NEWS</h1>
+            </div>
+            <div className="flex w-3/6 mx-auto justify-between text-xs">
+                {newsData.map((news) => (
+                    <div className=' hover:text-[red] w-[30%]'>
+                        <img src={news.image} alt='' />
+                        <p className='text-[#fff5] my-2'>20 jun 2021 </p>
+                        <p>{news.detail}</p>
+                        <a className='flex  items-center'>
+                            <div className='h-[0.2rem] w-[1rem] bg-[red] mr-2'></div>
+                            see more
+                        </a>
+                    </div>
+                ))}
             </div>
         </div>
     )
