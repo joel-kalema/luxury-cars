f39d79ba8316f8d1f02611f22ed7b0137872b8b2
joel <joelkalema63@gmail.com>
2023-01-06 15:45:38 +0200

    create a news component

---
+++ components/news.js
@@ -0,0 +1,11 @@
+export default function News () {
+    return (
+        <div className="py-10 md:py-28">
+            <div className="mx-auto w-2/6 mb-10">
+                <h1 className="text-2xl">News and Events</h1>
+                <div className="w-1/6 h-[0.2rem] bg-[red] mt-1"></div>
+                <h1 className="text-[#fff2] text-8xl font-bold mt-[-4rem]">NEWS</h1>
+            </div>
+        </div>
+    )
+}
