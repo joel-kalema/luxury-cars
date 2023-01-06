afa8fc591cfa7a198d35b8083996a4414f0768f2
joel <joelkalema63@gmail.com>
2023-01-06 15:44:27 +0200

    import the news component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Header from '../components/header'
 import HomePage from '../components/home'
 import About from '../components/about'
 import Cars from '../components/cars'
+import News from '../components/news'
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <HomePage />
             <About />
             <Cars />
+            <News />
         </div>
     )
 }
