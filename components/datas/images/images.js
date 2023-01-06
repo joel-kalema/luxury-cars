eec59006805ee0172c5efa007ad0dc6d4b7b78cf
joel <joelkalema63@gmail.com>
2023-01-06 15:10:08 +0200

    add logo for logos

---
+++ components/datas/images/images.js
@@ -0,0 +1,20 @@
+const images = [
+    {
+        logo: "images/logo/Lamborghini-Logo.png",
+        name: 'Lamborghini'
+    },
+    {
+        logo: "images/logo/Mercedes-Logo.png",
+        name: 'Mercedes'
+    },
+    {
+        logo: "images/logo/Nissan_logo.png",
+        name: 'Nissan'
+    },
+    {
+        logo: "images/logo/porsche.png",
+        name: 'porsche'
+    }
+]
+
+export default images;
