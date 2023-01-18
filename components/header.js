1225836b2fc4eefd450ff0647b9e8a4a65b5c1df
joel <joelkalema63@gmail.com>
2023-01-18 19:40:46 +0200

    add hover colors

--- components/header.js
+++ components/header.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Header (){
     return (
         <div className="flex justify-between fixed w-5/6 exp backdrop-blur-md p-4 left-24 bg-[#fff1] z-50">
             <div>LOGO</div>
             <ul className="flex w-2/6 justify-between text-sm">
-                <li className='cursor-pointer hover:text-[red]'>Home</li>
+                <li className='cursor-pointer hover:text-[red]'><Link href='/'>Home</Link></li>
                 <li className='cursor-pointer hover:text-[red]'>cars</li>
                 <li className='cursor-pointer hover:text-[red]'>About</li>
                 <li className='cursor-pointer hover:text-[red]'>Infos</li>
