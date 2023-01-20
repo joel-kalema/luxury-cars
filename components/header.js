c48ae7d46ce80b08659502996c7dc72f3f55f7bf
joel <joelkalema63@gmail.com>
2023-01-20 11:31:29 +0200

    add search button

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ export default function Header (){
 
     return (
         <div>
-            <div className="flex justify-between fixed w-5/6 exp backdrop-blur-md p-4 left-24 bg-[#fff1] z-50">
+            <div className="flex justify-between fixed w-5/6 exp backdrop-blur-md p-4 left-24 bg-[#0004] z-50">
                 <div>LOGO</div>
                 <ul className="flex w-2/6 justify-between text-sm">
                     <li className='cursor-pointer hover:text-[red]'><Link href='/'>Home</Link></li>
@@ -22,7 +22,7 @@ export default function Header (){
             </div>
             <div className={active}>
                 <div className='fixed top-0 left-0 flex justify-center items-center flex-col h-screen w-full bg-[#1B1A1C] z-50'>
-                    <AiOutlineClose onClick={navToggle}/>
+                    <AiOutlineClose onClick={navToggle} className='absolute top-12 right-12 text-xl hover:rotate-90 transition-all' />
                     <form>
                         <input className='text-4xl' type="text" name="search" id="search" placeholder="START TYPING..." maxlength="30"/>
                     </form>
