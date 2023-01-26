19421b0284028673a9d0140f5008731e5ae58f39
joel <joelkalema63@gmail.com>
2023-01-26 19:11:38 +0200

    create a lines component

---
+++ components/lines.js
@@ -0,0 +1,14 @@
+export default function Line () {
+    return (
+        <div className='absolute top-0 h-[40%] flex'>
+            <div className='w-[.5rem] bg-[red] h-[100%] mr-4'></div>
+            <div className='w-[.5rem] bg-[red] h-[90%] mr-4'></div>
+            <div className='w-[.5rem] bg-[red] h-[80%] mr-4'></div>
+            <div className='w-[.5rem] bg-[red] h-[70%] mr-4'></div>
+            <div className='w-[.5rem] bg-[red] h-[60%] mr-4'></div>
+            <div className='w-[.5rem] bg-[red] h-[50%] mr-4'></div>
+            <div className='w-[.5rem] bg-[red] h-[40%] mr-4'></div>
+            <div className='w-[.5rem] bg-[red] h-[30%]'></div>
+        </div>
+    )
+}
