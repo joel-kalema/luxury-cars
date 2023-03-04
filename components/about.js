3d2534a8741723de60e9401ddc9f6f4314bb52ac
joel <joelkalema63@gmail.com>
2023-03-04 13:14:41 +0200

    updated the style

--- components/about.js
+++ components/about.js
@@ -25,7 +25,7 @@ export default function About () {
                 <div className="w-1/6 h-[0.2rem] bg-[red] mt-1"></div>
                 <h1 className="text-[#fff2] text-8xl font-bold mt-[-4rem]">LUXURY</h1>
             </div>
-            <Link href='/paths/cars' className='ml-[68%] text-xs border-stone-400 border-[1px] py-2 px-4'>VIEW ALL</Link>
+            <Link href='/paths/cars' className='ml-[68%] view text-xs font-semibold py-2 px-4'>VIEW ALL</Link>
             <div className="flex w-3/6 mx-auto justify-between mt-10">
                 <ul>
                     {CarsData.map((car) => (
