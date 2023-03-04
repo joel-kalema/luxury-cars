cd694aab7f204dc8640d3a1b2e68f11b83259a34
joel <joelkalema63@gmail.com>
2023-03-04 13:15:31 +0200

    updated the React styles

--- pages/cars/[name].tsx
+++ pages/cars/[name].tsx
@@ -18,18 +18,23 @@ const router: NextRouter = useRouter();
                     <a href={car?.link} className='hover:text-[red]'>{car?.link}</a>
                 </div>
             </div>
-            <div className="h-screen z-20 flex mx-20 mt-20">
-                <div className="flex w-2/4">
-                    <div className="w-2/4 mt-10"><img src={car?.image[2]} alt={car?.name} /></div>
-                    <div className="w-2/4"><img src={car?.image[3]} alt={car?.name} /></div>
+            <div className="flex w-3/5 mx-auto mt-20 rounded-2xl overflow-hidden bg-[#141314] drop-shadow-xl">
+                <div className="flex w-2/5">
+                    <div><img src={car?.image[2]} alt={car?.name} /></div>
+                    {/* <div className="w-2/4"><img src={car?.image[3]} alt={car?.name} /></div> */}
                 </div>
-                <div className="w-2/4 pl-10">
-                    <div className="w-2/4 mb-28">
+                <div className="w-3/5 pl-10">
+                    <div className="w-2/4 mb-10 mt-10">
                         <h1 className="text-2xl">{car?.name}</h1>
                         <div className="w-1/6 h-[0.2rem] bg-[red] mt-1"></div>
-                        <h1 className="text-[#fff2] text-8xl font-bold mt-[-4rem]">LUXURY</h1>
+                        <h1 className="text-[#fff2] text-6xl font-bold mt-[-3rem]">LUXURY</h1>
                     </div>
-                    <p>{car?.detail}</p>
+                    <p className="w-4/5 text-sm">{car?.detail}</p>
+                    <h2 className="text-[#fff8] mt-10">Driving a luxurious</h2>
+                    <div className="flex items-center mt-40 ml-40">
+                        <div className="w-2/6 h-[0.05rem] bg-[red] mt-1 mr-2"></div>
+                        <div className="w-[2rem] h-[2rem] mt-1 border-[0.05rem] rounded-full border-solid border-red-600"></div>
+                    </div>
                 </div>
             </div>
             <div className="py-32">
