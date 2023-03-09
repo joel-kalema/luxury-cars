24921f5a1c021999194b04feb0b51409b6376a81
joel <joelkalema63@gmail.com>
2023-03-09 18:51:02 +0200

    Updated the style

--- pages/cars/[name].tsx
+++ pages/cars/[name].tsx
@@ -1,5 +1,6 @@
 import { NextRouter, useRouter } from "next/router";
-import Link from "next/link";
+import { GiCarWheel } from 'react-icons/gi';
+import { AiOutlineArrowDown } from 'react-icons/ai';
 import getCar from "../../components/datas/cars";
 
 export default function Par (){
@@ -12,17 +13,16 @@ const router: NextRouter = useRouter();
             <div className="h-screen truncate detail_home truncate">
                 <img src={car?.image[1]} alt={car?.name} className='w-full'/>
             </div>
-            <div className="absolute top-0 px-60 text-center flex justfy-center items-center h-screen">
-                <div >
-                    <h1 className="font-bold text-6xl">{car?.name}</h1>
-                    <a href={car?.link} className='hover:text-[red]'>{car?.link}</a>
+            <div className="absolute top-0 text-center flex justfy-center items-center h-screen w-full">
+                <div className="mx-auto">
+                    <h1 className="font-bold text-6xl mb-10">{car?.name}</h1>
+                    <h2 className="mb-4">Tcheck the {car?.name}'s official website</h2>
+                    <a href={car?.link} className='text-[red]'>{car?.link}</a>
+                    <AiOutlineArrowDown className='animate-bounce w-9 h-9 text-[red] mx-auto mt-14'/>
                 </div>
             </div>
-            <div className="flex w-3/5 mx-auto mt-20 rounded-2xl overflow-hidden bg-[#141314] drop-shadow-xl">
-                <div className="flex w-2/5">
-                    <div><img src={car?.image[2]} alt={car?.name} /></div>
-                    {/* <div className="w-2/4"><img src={car?.image[3]} alt={car?.name} /></div> */}
-                </div>
+            <div className="flex w-3/5 mx-auto mt-20 rounded-2xl overflow-hidden bg-[#141314] drop-shadow-xl h-[30rem]">
+                <div className="flex w-2/5"><img src={car?.image[2]} alt={car?.name} /></div>
                 <div className="w-3/5 pl-10">
                     <div className="w-2/4 mb-10 mt-10">
                         <h1 className="text-2xl">{car?.name}</h1>
@@ -31,9 +31,10 @@ const router: NextRouter = useRouter();
                     </div>
                     <p className="w-4/5 text-sm">{car?.detail}</p>
                     <h2 className="text-[#fff8] mt-10">Driving a luxurious</h2>
-                    <div className="flex items-center mt-40 ml-40">
-                        <div className="w-2/6 h-[0.05rem] bg-[red] mt-1 mr-2"></div>
-                        <div className="w-[2rem] h-[2rem] mt-1 border-[0.05rem] rounded-full border-solid border-red-600"></div>
+                    
+                    <div className="flex items-center mt-16 ml-64">
+                        <div className="w-2/6 h-[0.05rem] bg-[red] mt-4 mr-2"></div>
+                        <GiCarWheel className="text-[red] text-4xl rotate"/>
                     </div>
                 </div>
             </div>
