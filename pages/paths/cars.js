e0960fb40ef0075875c5afe7be375fafff493b4c
joel <joelkalema63@gmail.com>
2023-03-12 20:26:05 +0200

    update the design

--- pages/paths/cars.js
+++ pages/paths/cars.js
@@ -1,11 +1,9 @@
-import Import from '../import';
 import Home from './home';
 import AllCars from './AllCars'
 
 export default function Cars() {
     return (
         <div className='bg-[#1B1A1C]'>
-            <Import.Header />
             <Home />
             <AllCars />
         </div>
