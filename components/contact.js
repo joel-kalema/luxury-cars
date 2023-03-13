c2328b7c8d755e525d639970ecdf1160d4977a8f
joel <joelkalema63@gmail.com>
2023-03-13 17:25:48 +0200

    update the design

--- components/contact.js
+++ components/contact.js
@@ -1,18 +1,9 @@
+
+
 export default function Contacts () {
     return (
-        <div className="py-10 md:py-20 bg-[#0004] flex">
-            <div>
-                <div className="mx-auto w-4/6 mb-10">
-                    <h1 className="text-2xl">Contact Directly</h1>
-                    <div className="w-1/6 h-[0.2rem] bg-[red] mt-1"></div>
-                    <h1 className="text-[#fff2] text-8xl font-bold mt-[-4rem]">CONTACT</h1>
-                </div>
-                <div className="mx-auto w-2/6 text-xs">
-                    <p>Kigali, Rwanda</p>
-                    <p>+243 972 355 737</p>
-                    <p>joelkalema63@gmail.com</p>
-                </div>
-           </div>
+        <div className="py-10 md:py-20 flex">
+           
             
         </div>
     )
