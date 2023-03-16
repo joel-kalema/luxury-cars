0c319d319c4b763b7ded138516a90b8ab3cf1963
joel <joelkalema63@gmail.com>
2023-03-16 03:55:36 +0200

    update the design

--- components/contact.js
+++ components/contact.js
@@ -2,9 +2,8 @@
 
 export default function Contacts () {
     return (
-        <div className="py-10 md:py-20 flex">
-           
-            
+        <div>
+               
         </div>
     )
 }
