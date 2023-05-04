216c8b6f2fe8a43ec63f6d8f231b0602c4f9a47b
joel-kalema <joelkalema63@gmail.com>
2023-05-04 23:42:30 +0200

    update styles

--- components/wide.js
+++ components/wide.js
@@ -7,11 +7,12 @@ export default function Wide () {
             <div className="experience p-20">
                 <h1 className="text-5xl mt-20">A Great Experience</h1>
                 <h1 className="text-5xl my-4">Is Waiting Four You</h1>
-                <div className="mt-10 w-2/4 exp backdrop-blur-md bg-[#0004] p-10">
+                <div className="mt-10 w-2/4 exp backdrop-blur-md bg-[#0004] p-10  mb-[-9rem]">
                     <p>For those who love adrenaline, renting a supercar is not a question of appearance, but of passion. Don&lsquo;t limit your dream, experience the thrill of driving the best sport cars on the market with Montecarlo Luxury Cars.</p>
                     <div className="flex items-center mt-10">
                         <div className="w-1/6 h-[0.05rem] bg-[red] mt-1 mr-2"></div>
                         <div className="w-[1rem] h-[1rem] mt-1 border-[0.05rem] rounded-full border-solid border-red-600"></div>
+                        <p className="text-xs text-[red] ml-4">Experience</p>
                     </div>
                 </div>
             </div>
