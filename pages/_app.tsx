42aeb5948ebee8ac004a4f34ded2d264cdfff075
joel-kalema <joelkalema63@gmail.com>
2023-05-06 12:41:34 +0200

    Added animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,10 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <>
         <Header />
         <Component {...pageProps} />
         <Footer />
-    </div>
+    </>
   )
 }
