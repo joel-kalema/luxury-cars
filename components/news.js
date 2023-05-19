ab0a3b2720567b2aeb759e40149ed82addeb027e
joel-kalema <joelkalema63@gmail.com>
2023-05-19 21:34:51 +0200

    Add responsiveness

--- components/news.js
+++ components/news.js
@@ -4,10 +4,10 @@ import { MdOutlineModeComment } from 'react-icons/md';
 export default function News() {
     return (
         <div className="py-10 md:py-52">
-            <div className="mx-auto w-2/6 mb-10">
-                <h1 className="text-2xl">News and Events</h1>
+            <div className="mx-auto w-4/6 md:w-2/6 mb-28">
+                <h1 className="text-sm md:text-2xl">News and Events</h1>
                 <div className="w-1/6 h-[0.2rem] bg-[red] mt-1"></div>
-                <h1 className="text-[#fff1] text-9xl font-bold mt-[-6rem]">NEWS</h1>
+                <h1 className="text-[#fff1] text-6xl md:text-9xl font-bold mt-[-3rem] md:mt-[-6rem]">NEWS</h1>
             </div>
             <div className="flex w-3/6 mx-auto justify-between text-xs mt-20">
                 {newsData.map((news) => (
