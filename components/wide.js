238c1a61e32bc460c5a63aa0ea73f4b1d3201e54
joel-kalema <joelkalema63@gmail.com>
2023-05-19 21:35:13 +0200

    Add responsiveness

--- components/wide.js
+++ components/wide.js
@@ -1,10 +1,10 @@
 export default function Wide () {
     return (
-        <div className='pt-40 relative'>
-            <div className="experience p-20">
-                <h1 className="text-5xl mt-20">A Great Experience</h1>
-                <h1 className="text-5xl my-4">Is Waiting Four You</h1>
-                <div className="mt-10 w-2/4 exp backdrop-blur-md bg-[#0004] p-10  mb-[-9rem]">
+        <div className='pt-20 md:pt-40 relative'>
+            <div className="experience p-6 md:p-20">
+                <h1 className="text-2xl md:text-5xl mt-20">A Great Experience</h1>
+                <h1 className="text-2xl md:text-5xl my-4">Is Waiting Four You</h1>
+                <div className="mt-10 w-4/4 md:w-2/4 exp backdrop-blur-md bg-[#0004] p-10  mb-[-9rem]">
                     <p>For those who love adrenaline, renting a supercar is not a question of appearance, but of passion. Don&lsquo;t limit your dream, experience the thrill of driving the best sport cars on the market with Montecarlo Luxury Cars.</p>
                     <div className="flex items-center mt-10">
                         <div className="w-1/6 h-[0.05rem] bg-[red] mt-1 mr-2"></div>
