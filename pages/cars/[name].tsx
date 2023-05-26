ae0bd5faf034011fad876262ad0808d3b19da81a
joel-kalema <joelkalema63@gmail.com>
2023-05-26 15:08:14 +0200

    update the design

--- pages/cars/[name].tsx
+++ pages/cars/[name].tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import LoadPage from './load-page'
 import { NextRouter, useRouter } from "next/router";
 import { GiCarWheel } from 'react-icons/gi';
 import { AiOutlineArrowDown } from 'react-icons/ai';
@@ -32,15 +33,15 @@ export default function Par() {
             <div className="absolute top-0 text-center flex justfy-center items-center h-screen w-full">
                 <div className="mx-auto">
                     <div className='mb-6 flex flex-col items-center'>
-                        <div className='h-[.4rem] w-[5rem] bg-[red] mr-28 mb-4'></div>
+                        <div className='h-[.4rem] w-[8rem] liner1 mr-28 mb-4'></div>
                         <h2 className='text-center font-bold text-6xl uppercase datail-name'>{car?.name}</h2>
-                        <div className='h-[.4rem] w-[5rem] bg-[red] ml-28 mt-4'></div>
+                        <div className='h-[.4rem] w-[8rem] liner2 ml-28 mt-4'></div>
                     </div>
-                    <h2 className="mb-4 text-2xl">Get more details about {car?.name}</h2>
+                    <h2 className="mb-4 text-xl">Get more details about {car?.name}</h2>
                     <AiOutlineArrowDown className='animate-bounce w-9 h-9 text-[red] mx-auto mt-10' />
                 </div>
             </div>
-            <div className="flex max-w-[50rem] mx-auto rounded-2xl overflow-hidden bg-[#141314] drop-shadow-xl h-[30rem] mt-[-4rem]">
+            <div className="flex max-w-[50rem] mx-auto rounded-2xl overflow-hidden bg-[#141314] drop-shadow-xl h-[30rem] mt-[-4rem]" data-aos="zoom-in-up">
                 <div className="flex w-2/5"><img src={car?.image[2]} alt={car?.name} /></div>
                 <div className="w-3/5 pl-10">
                     <div className="w-2/4 mb-10 mt-10">
@@ -49,7 +50,7 @@ export default function Par() {
                         <h1 className="text-[#fff2] text-6xl font-bold mt-[-3rem]">LUXURY</h1>
                     </div>
                     <p className="w-4/5 text-sm">{car?.detail}</p>
-                
+
                     <h2 className="mb-4 mt-10">Tcheck the {car?.name}&lsquo;s official website</h2>
                     <a href={car?.link} className='text-[red]'>{car?.name}</a>
 
@@ -60,20 +61,24 @@ export default function Par() {
                 </div>
             </div>
             <div className="py-32">
-                <h1 className="font-bold text-3xl text-center">GALERY</h1>
-                <div className="gallery">
+                <h1 className="font-bold text-3xl text-center mb-6">GALERY</h1>
+                <div className="w-[8rem] h-[2px] liner1 mx-auto"></div>
+                <p className="text-center tracking-[1rem] text-sm font-bold text-[red] my-2">Luxury car</p>
+                <div className="w-[8rem] h-[2px] liner2 mx-auto"></div>
+                <div className="gallery" data-aos="fade-up">
                     {url?.map((value) => (
-                        <div className="img mb-4">
-                            <img src={value.urls.small} alt='image' className="gall-image" />
-                            <div className="p-2 exp backdrop-blur-md bg-[#0009] image-detail">
-                                <div className="mb-4 flex items-end justify-center">
-                                    <FcLike className="text-3xl" />
-                                    <h1 className="ml-2">{value.likes}</h1>
+                            <div className="img mb-4" data-aos="fade-up">
+                                <img src={value.urls.small} alt='image' className="gall-image" />
+                                <div className="p-2 exp backdrop-blur-md bg-[#0009] image-detail">
+                                    <div className="mb-4 flex items-end justify-center">
+                                        <FcLike className="text-3xl" />
+                                        <h1 className="ml-2">{value.likes}</h1>
+                                    </div>
+                                    <p className="text-sm text-center w-4/5">{value.alt_description}</p>
                                 </div>
-                                <p className="text-sm text-center w-4/5">{value.alt_description}</p>
                             </div>
-                        </div>
-                    ))}
+                        ))
+                    }
                 </div>
             </div>
         </div>
