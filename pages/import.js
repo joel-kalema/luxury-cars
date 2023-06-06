3fd752c4c8ae73c64366ae548ac536c38f9bd078
joel-kalema <joelkalema63@gmail.com>
2023-06-06 23:51:06 +0200

    fix error

---
+++ pages/import.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/header';
+import { CarsData } from '../components/datas/cars';
+
+const Import ={
+    Header,
+    CarsData
+}
+
+export default Import
